974b648e4b9d38e45afdb81783f59b0cacb72094
David De La Rosa <davidrr261096@hotmail.com>
2026-04-28 13:30:16 -0400

    Add routes link to navbar and add paths file for tsconfig

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { HousesPage } from './pages/houses/houses.page';
+import { MarkersPage } from './pages/markers/markers.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'map',
+    component: HomePage,
+    title: 'Map',
+  },
+  {
+    path: 'houses',
+    component: HousesPage,
+    title: 'Houses',
+  },
+  {
+    path: 'markers',
+    component: MarkersPage,
+    title: 'Markers',
+  },
+  {
+    path: '**',
+    redirectTo: 'map',
+  },
+];
--- src/app/shared/components/navbar/index.ts
+++ src/app/shared/components/navbar/index.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { routes } from '../../../app.routes';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-index',
-  imports: [],
+  selector: 'navbar-app',
+  imports: [TitleCasePipe, RouterLink],
   templateUrl: './index.html',
 })
-export class Index {}
+export class Navbar {
+  private router = inject(Router);
+
+  appRoutes = routes
+    .filter((route) => route.title)
+    .map((route) => ({ title: route.title as string, path: route.path as string }));
+
+  currentUrl = toSignal(
+    this.router.events.pipe(
+      filter((e) => e instanceof NavigationEnd),
+      map(() => this.router.url.slice(1)),
+    ),
+  );
+}
