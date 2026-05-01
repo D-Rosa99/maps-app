ae72e5790d2139d833a63edb790ab0505ce4aa3c
David De La Rosa <davidrr261096@hotmail.com>
2026-04-30 23:48:44 -0400

    Add google.maps as global type and create zoom map signal

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
 import { environment } from '@env/environment';
 import { setOptions, importLibrary } from '@googlemaps/js-api-loader';
 
@@ -11,20 +11,41 @@ import { setOptions, importLibrary } from '@googlemaps/js-api-loader';
       width: 100vw;
       height: calc(100vh - 64px);
     }
+
+    #controls {
+      background-color: white;
+      padding: 10px;
+      border-radius: 5px;@types/google.maps
+      position: fixed;
+      bottom: 20px;
+      right: 20px;
+      z-index: 9999;
+      box-shadow: 0 0 10px 0 rgba(0,0,0,0.1);
+      border: 1px solid #e2e8f0;
+      width: 250px;
+    }
+
   `,
 })
 export class MapPage implements AfterViewInit {
   divEl = viewChild<ElementRef>('mapEl');
+  zoomMap = signal<number>(8)
+  mapSignal = signal<google.maps.Map | null>(null);
+
+  zoomEffect = effect(() => {
+    if( !this.mapSignal()) return
+
+    this.mapSignal()?.setZoom(this.zoomMap())
+  })
 
   async ngAfterViewInit() {
     if (!this.divEl()?.nativeElement) return;
     const mapEl = this.divEl()!.nativeElement;
-
     setOptions({ key: environment.GMAP_KEY });
 
     const { Map } = await importLibrary('maps');
     const map = new Map(mapEl, {
-      zoom: 8,
+      zoom: this.zoomMap(),
       minZoom: 3,
       maxZoom: 18,
       center: { lat: 40.7128, lng: -74.006 },
@@ -38,5 +59,18 @@ export class MapPage implements AfterViewInit {
         strictBounds: true,
       },
     });
+
+    this.mapEventListener(map)
+  }
+
+  mapEventListener(mapInstance: google.maps.Map){
+    mapInstance.addListener('zoom_changed', () => {
+      const mapEL = this.divEl()?.nativeElement;
+      console.log('newZoom', mapEL.getZoom())
+      // this.zoomMap.set(newZoom)
+    })
+
+
+    this.mapSignal.set(mapInstance)
   }
 }
